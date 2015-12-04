1fc3c9b600650b5fe97d83c081ce2788c69d5a8c
jpain <james.pain@veinteractive.com>
2015-12-04 23:30:01 +0000

    Init project for StringCalculatorTDDKata1 with C# and Unit Test projects

---
+++ TDDKata/StringCalculatorTDDKata1-Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculatorTDDKata1_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
