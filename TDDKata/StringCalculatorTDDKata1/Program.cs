94eb0f2042ac85868d4b259c301ceb7d95d3352b
JPain <jamesepain@gmail.com>
2015-12-05 16:58:27 +0000

    Passed Defined_Delimiters_Returns_Sum

--- TDDKata/StringCalculatorTDDKata1/Program.cs
+++ TDDKata/StringCalculatorTDDKata1/Program.cs
@@ -17,8 +17,23 @@ namespace StringCalculatorTDDKata1
             if (string.IsNullOrWhiteSpace(input))
                 return 0;
 
-            var stringSeparators = new char[] { ',', '\n'};
-            var sNumbers = input.Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries);
+            var stringSeparators = new List<String> { ",", "\n"};
+
+            if (input.Length > 5)
+            {
+                if (input.Substring(0, 2) == "//")
+                {
+                    var delimiter = input.Substring(2, input.IndexOf('\n') - 2);
+
+                    if (!string.IsNullOrEmpty(delimiter))
+                    {
+                        stringSeparators.Add(delimiter);
+                    }
+
+                    input = input.Substring(input.IndexOf('\n') + 1);
+                }
+            }
+            var sNumbers = input.Split(stringSeparators.ToArray(), StringSplitOptions.RemoveEmptyEntries);
             
             var result = 0;
 
