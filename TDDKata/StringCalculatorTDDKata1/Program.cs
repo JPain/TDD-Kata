fe304ff898298909c6b036bfc3be3c6ca5b8b1d0
jpain <james.pain@veinteractive.com>
2015-12-05 00:15:00 +0000

    Passed Add_With_Empty_String_Returns_0

--- TDDKata/StringCalculatorTDDKata1/Program.cs
+++ TDDKata/StringCalculatorTDDKata1/Program.cs
@@ -14,6 +14,10 @@ namespace StringCalculatorTDDKata1
 
         public int Add(string numbers)
         {
+            if (string.IsNullOrWhiteSpace(numbers))
+            {
+                return 0;
+            }
             throw new NotImplementedException();
         }
     }
