f72693f799cc4884fc099c9b5d6b310ec13a237c
jpain <james.pain@veinteractive.com>
2015-12-05 00:55:52 +0000

    Removing 'Add_With_' from Test Method names to simplify

--- TDDKata/StringCalculatorTDDKata1-Tests/AddTests.cs
+++ TDDKata/StringCalculatorTDDKata1-Tests/AddTests.cs
@@ -16,28 +16,28 @@ namespace StringCalculatorTDDKata1_Tests
         }
 
         [TestMethod]
-        public void Add_With_Empty_String_Returns_0()
+        public void Empty_String_Returns_0()
         {
             var result = _stringCalculator.Add("");
             Assert.AreEqual(result, 0);
         }
 
         [TestMethod]
-        public void Add_With_One_Number_String_Returns_0()
+        public void One_Number_String_Returns_0()
         {
             var result = _stringCalculator.Add("4");
             Assert.AreEqual(result, 4);
         }
 
         [TestMethod]
-        public void Add_With_Two_Comma_Sepearted_Numbers_Returns_Sum()
+        public void Two_Comma_Sepearted_Numbers_Returns_Sum()
         {
             var result = _stringCalculator.Add("2,4");
             Assert.AreEqual(result, 6);
         }
 
         [TestMethod]
-        public void Add_With_Three_Comma_Seperated_Numbers_Returns_Sum()
+        public void Three_Comma_Seperated_Numbers_Returns_Sum()
         {
             var result = _stringCalculator.Add("2,4,6");
             Assert.AreEqual(result, 12);
