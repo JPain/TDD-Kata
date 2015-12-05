7615965a37ef395df94b0b3e2de3d72063b4db6b
jpain <james.pain@veinteractive.com>
2015-12-05 00:12:52 +0000

    Added test Add_With_Empty_String_Returns_0

---
+++ TDDKata/StringCalculatorTDDKata1-Tests/AddTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculatorTDDKata1;
+
+namespace StringCalculatorTDDKata1_Tests
+{
+    [TestClass]
+    public class AddTests
+    {
+        private StringCalculator StringCalculator;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            StringCalculator = new StringCalculator();
+        }
+
+        [TestMethod]
+        public void Add_With_Empty_String_Returns_0()
+        {
+            var result = StringCalculator.Add("");
+            Assert.AreEqual(result, 0);
+        }
+    }
+}
